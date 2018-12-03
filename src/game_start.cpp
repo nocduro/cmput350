52756c2ad46f6b2df3be9af026c4bdcd85ae0bd7
Mackenzie Hauck <mhauck@live.ca>
2018-12-03 15:24:19 -0700

    refactor to use CountUnitType function

--- src/game_start.cpp
+++ src/game_start.cpp
@@ -9,7 +9,7 @@ sc::result GameStart_Refinery::react(const StepEvent& event) {
 	auto observation = context<StateMachine>().Observation;
     Units Refinerys = observation()->GetUnits(Unit::Alliance::Self, IsUnit(UNIT_TYPEID::TERRAN_REFINERY));
         
-    if (observation()->GetUnits(Unit::Alliance::Self, IsUnit(UNIT_TYPEID::TERRAN_REFINERY)).size() >= 1){
+    if (CountUnitType(observation, UNIT_TYPEID::TERRAN_REFINERY) >= 1){
         if(FarmGas(actions,observation)){
             std::cout <<"done farming gas" << std::endl;
         }
@@ -17,14 +17,13 @@ sc::result GameStart_Refinery::react(const StepEvent& event) {
     
     
     
-    if (observation()->GetUnits(Unit::Alliance::Self, IsUnit(UNIT_TYPEID::TERRAN_REFINERY)).size() > 1){
+    if (CountUnitType(observation, UNIT_TYPEID::TERRAN_REFINERY) > 1){
         for (const auto& refinery: Refinerys){
             if (refinery->assigned_harvesters == refinery->ideal_harvesters){
                 return transit<GameStart_BuildArmy>();
             }
         }
-    }
-    if (observation()->GetUnits(Unit::Alliance::Self, IsUnit(UNIT_TYPEID::TERRAN_REFINERY)).size() < 1){
+    } else {
         TryBuildRefinery(actions, observation);
     }
 	return discard_event();
@@ -34,13 +33,13 @@ sc::result GameStart_BuildBarracks::react(const StepEvent& event) {
 	auto actions = context<StateMachine>().Actions;
 	auto observation = context<StateMachine>().Observation;
 
-	if (observation()->GetUnits(Unit::Alliance::Self, IsUnit(UNIT_TYPEID::TERRAN_SUPPLYDEPOT)).size() < 1) {
+	if (CountUnitType(observation, UNIT_TYPEID::TERRAN_SUPPLYDEPOT) < 1) {
 		TryBuildSupplyDepot(actions, observation);
 		return discard_event();
 	}
     
 
-	if (observation()->GetUnits(Unit::Alliance::Self, IsUnit(UNIT_TYPEID::TERRAN_BARRACKS)).size() > 1) {
+	if (CountUnitType(observation, UNIT_TYPEID::TERRAN_BARRACKS) > 1) {
 		return transit<GameStart_Refinery>();
 	}
 
@@ -51,15 +50,8 @@ sc::result GameStart_BuildBarracks::react(const StepEvent& event) {
 // Build an army until we have 10 marines, then try an 
 // early rush attack
 sc::result GameStart_BuildArmy::react(const StepEvent& event) {
-	auto observation = context<StateMachine>().Observation();
-	int num_marines = 0;
-	Units my_units = observation->GetUnits(Unit::Alliance::Self);
-	for (const auto unit : my_units) {
-		if (unit->unit_type == UNIT_TYPEID::TERRAN_MARINE) {
-			++num_marines;
-		}
-	}
-	if (num_marines > 10) {
+	auto observation = context<StateMachine>().Observation;
+	if (CountUnitType(observation, UNIT_TYPEID::TERRAN_MARINE) > 10) {
 		return transit<EarlyRushState>();
 	}
 	else {
