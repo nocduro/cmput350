bc4557c6349a8bf6f1fa468b6434f6b96744453c
Erik <ligai@ualberta.ca>
2018-12-04 02:26:58 -0700

    orbital command, function stubs

--- src/bot.cpp
+++ src/bot.cpp
@@ -23,18 +23,17 @@ public:
 		std::cout << "Can I get a pogchamp in the chat?" << std::endl;
 		const GameInfo& game_info = Observation()->GetGameInfo();
 		playerpos = getPlayerPos(game_info.enemy_start_locations);
-		std::cout << "We are starting at: (" <<playerpos.x << ", " << playerpos.y << ")"<< std::endl;
+		std::cout << "We are starting at: (" << playerpos.x << ", " << playerpos.y << ")"<< std::endl;
 	}
 
 	virtual void OnStep() final {
+		// called every step
 		TryBuildSupplyDepot();
 		TryBuildBarracks();
+		TryBuildRefinery();
         TryFarmGas();
         //TryUpdgradeBarracks();
         TryBuildFactory();
-        
-        
-        
 	}
 
 	virtual void OnUnitIdle(const Unit* unit) final {
@@ -43,9 +42,16 @@ public:
 		
 		switch (unit->unit_type.ToType()) {
 
-            // if command center is idle, makes scvs (should be running constantly)
+            // if command center is idle, makes scvs
     		case UNIT_TYPEID::TERRAN_COMMANDCENTER: {
                 size_t scvCount = CountUnitType(UNIT_TYPEID::TERRAN_SCV);
+
+				// when supply hits 19, we want to stop scv production and call orbital
+				if (observation->GetFoodUsed() >= 19) {
+					Actions()->UnitCommand(unit, ABILITY_ID::MORPH_ORBITALCOMMAND);
+					std::cout << "Orbital command started" << std::endl;
+				}
+
                 if (scvCount <= 30){
                     Actions()->UnitCommand(unit, ABILITY_ID::TRAIN_SCV);
                 }
@@ -59,6 +65,12 @@ public:
     			const Unit* gas_target = FindNearestObject(unit->pos,UNIT_TYPEID::NEUTRAL_VESPENEGEYSER);
                 const Unit* refinery_target = FindNearestObject(unit->pos,UNIT_TYPEID::TERRAN_REFINERY);
                 Units Refinerys = Observation()->GetUnits(Unit::Alliance::Neutral, IsUnit(UNIT_TYPEID::TERRAN_REFINERY));
+
+				// if scv is idle and supply is equal to 19, build a second command center
+				if (observation->GetFoodUsed() >= 19) {
+					Actions()->UnitCommand(unit, ABILITY_ID::BUILD_COMMANDCENTER, mineral_target);
+				}
+
     			if (!mineral_target) {
                     // TryBuildRefinery();
     				// Actions()->UnitCommand(unit, ABILITY_ID::SMART, gas_target); // send idle worker to gas
@@ -83,7 +95,7 @@ public:
                 }*/
                 
     			else {
-                    if (countSCV <14){
+                    if (countSCV < 14){
                         Actions()->UnitCommand(unit, ABILITY_ID::SMART, mineral_target);// send idle worker to mineral patch
                     }
                     else{
@@ -95,13 +107,13 @@ public:
     		case UNIT_TYPEID::TERRAN_BARRACKS: {
                 size_t counttech_lab = CountUnitType(UNIT_TYPEID::TERRAN_TECHLAB);
                 if (counttech_lab < 1 && !haveTechLab){
-                    std::cout << "here" << std::endl;
+                    std::cout << "Build TechLab" << std::endl;
                     Actions()->UnitCommand(unit, ABILITY_ID::BUILD_TECHLAB);
-                    std::cout << "here2" << std::endl;
+                    // std::cout << "here2" << std::endl;
                     if (counttech_lab >=1){
                         haveTechLab = true;
                     }
-                }else{
+                } else {
                     //BuildOrder(unit);
                 }
     			//Actions()->UnitCommand(unit, ABILITY_ID::TRAIN_MARINE);
@@ -141,6 +153,30 @@ public:
 
 		}
 	}
+
+	virtual void OnUnitCreated(const Unit* unit) final {
+
+	}
+
+	virtual void OnUpgradeCompleted(UpgradeID) {
+
+	}
+
+	virtual void OnBuildingConstructionComplete(const Unit*) {
+
+	}
+
+	// Called when an enemy unit enters view
+    // param unit The unit entering vision.
+    virtual void OnUnitEnterVision(const Unit*) {
+
+	}
+
+
+
+
+
+
 private:
 	//attacks early game with marinecount marines to nearest enemy
 	void earlyrush(size_t marinecount, int enemypos) {
@@ -176,24 +212,26 @@ private:
 		return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
 	}
 
+	// returns the number of a given unit type
 	size_t CountUnitType(UNIT_TYPEID unit_type) {
 		return Observation()->GetUnits(Unit::Alliance::Self, IsUnit(unit_type)).size();
 	}
 
+	// attempts to build a structure of given type
+	// default unit to build is scv
 	bool TryBuildStructure(ABILITY_ID ability_type_for_structure, UNIT_TYPEID unit_type = UNIT_TYPEID::TERRAN_SCV) {
 		const ObservationInterface* observation = Observation();
 
-		// If a unit already is building a supply structure of this type, do nothing.
+		// If any unit already is building a supply structure of this type, do nothing.
 		// Also get an scv to build the structure.
 		const Unit* unit_to_build = nullptr;
-		Units units = observation->GetUnits(Unit::Alliance::Self);
+		Units units = observation->GetUnits(Unit::Alliance::Self); // all units
 		for (const auto& unit : units) {
 			for (const auto& order : unit->orders) {
 				if (order.ability_id == ability_type_for_structure) {
 					return false;
 				}
 			}
-
 			if (unit->unit_type == unit_type) {
 				unit_to_build = unit;
 			}
@@ -202,10 +240,11 @@ private:
 		float rx = GetRandomScalar();
 		float ry = GetRandomScalar();
 
+		// if the structure type we want to build is a refinery, find nearest geyser and build
         if (ability_type_for_structure == ABILITY_ID::BUILD_REFINERY) {
             Actions()->UnitCommand(unit_to_build, ability_type_for_structure, FindNearestObject(unit_to_build->pos,UNIT_TYPEID::NEUTRAL_VESPENEGEYSER));
         } else {
-		  Actions()->UnitCommand(unit_to_build,
+		  	Actions()->UnitCommand(unit_to_build,
 			ability_type_for_structure,
 			Point2D(unit_to_build->pos.x + rx * 15.0f, unit_to_build->pos.y + ry * 15.0f));
         }
@@ -217,31 +256,37 @@ private:
 	bool TryBuildSupplyDepot() {
 		const ObservationInterface* observation = Observation();
 
+		// first supply depot built at 14 supply
         if (observation->GetFoodUsed() == 14) {
-            // std::cout << "BUILD SUPPLY DEPOT" << std::endl;
+            std::cout << "BUILT SUPPLY DEPOT" << std::endl;
+			std::cout << observation->GetFoodUsed() << std::endl;
             return TryBuildStructure(ABILITY_ID::BUILD_SUPPLYDEPOT);
         }
 
 		// If we are not supply capped, don't build a supply depot.
-		if (observation->GetFoodUsed() <= observation->GetFoodCap() - 2)
+		if (observation->GetFoodUsed() <= observation->GetFoodCap() - 2) {
 			return false;
+		}
 
         // std::cout << observation->GetFoodUsed() << " " << observation->GetFoodCap() << std::endl;
 
 		// Try and build a depot. Find a random SCV and give it the order.
-		return TryBuildStructure(ABILITY_ID::BUILD_SUPPLYDEPOT);
+		// return TryBuildStructure(ABILITY_ID::BUILD_SUPPLYDEPOT);
+		return false;
 	}
 
     // attempts to build a refinery on gas patch
     bool TryBuildRefinery() {
         const ObservationInterface* observation = Observation();
 
-        if (observation->GetFoodUsed() == 14 || observation->GetFoodUsed() == 16) {
-            // std::cout << "BUILD REFINERY" << std::endl;
+        if (observation->GetFoodUsed() == 15) {
+            std::cout << "BUILT REFINERY" << std::endl;
+			std::cout << observation->GetFoodUsed() << std::endl;
             return TryBuildStructure(ABILITY_ID::BUILD_REFINERY);
         }
 
-        return TryBuildStructure(ABILITY_ID::BUILD_REFINERY);
+        // return TryBuildStructure(ABILITY_ID::BUILD_REFINERY);
+		return false;
     }
 	
     // finds the nearest mineral patch to our current base
@@ -270,17 +315,20 @@ private:
 			return false;
 		}
 
-        // build first barracks only when supply is at 16
+        // build first barracks when supply is at 16
         if (observation->GetFoodUsed() == 16) {
-            // std::cout << "BUILD BARRACKS" << std::endl;
+            std::cout << "BUILD BARRACKS" << std::endl;
+			std::cout << observation->GetFoodUsed() << std::endl;
+			TryBuildStructure(ABILITY_ID::BUILD_BARRACKS);
         }
 
         // if we have more than 1 barracks, don't build anymore
-		if (CountUnitType(UNIT_TYPEID::TERRAN_BARRACKS) >= 1) {
-			return false;
-		}
+		// if (CountUnitType(UNIT_TYPEID::TERRAN_BARRACKS) >= 1) {
+		// 	return false;
+		// }
 
-		return TryBuildStructure(ABILITY_ID::BUILD_BARRACKS); // conditions were passed and we delegate to TryBuildStructure()
+		// return TryBuildStructure(ABILITY_ID::BUILD_BARRACKS); // conditions were passed and we delegate to TryBuildStructure()
+		return false;
 	}
         
         
